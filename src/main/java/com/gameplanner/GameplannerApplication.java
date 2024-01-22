4140df9a66e1ef1bc4cb5ba478771c994c52d784
alsduq1117 <alsduq1117@naver.com>
2024-01-22 23:02:06 +0900

    Initial commit

---
+++ src/main/java/com/gameplanner/GameplannerApplication.java
@@ -0,0 +1,13 @@
+package com.gameplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameplannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameplannerApplication.class, args);
+    }
+
+}
