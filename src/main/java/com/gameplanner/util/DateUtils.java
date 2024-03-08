17ba93693fa8a5f7a635819c95e1ddb72c5eabe1
alsduq1117 <alsduq1117@naver.com>
2024-03-08 17:41:38 +0900

    feat : utils 년도와 월을 유닉스 시간으로 변환하는 메소드 추가

--- src/main/java/com/gameplanner/util/DateUtils.java
+++ src/main/java/com/gameplanner/util/DateUtils.java
@@ -3,6 +3,7 @@ package com.gameplanner.util;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtils {
@@ -11,4 +12,17 @@ public class DateUtils {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
         return date.format(formatter);
     }
+
+
+    // 년도와 월을 유닉스 시간으로 변환하는 메소드
+    public static long toUnixTime(int year, int month) {
+        return LocalDateTime.of(year, month, 1, 0, 0)
+                .toEpochSecond(ZoneOffset.UTC);
+    }
+
+    public static LocalDateTime getNextMonth(int year, int month) {
+        LocalDateTime dateTime = LocalDateTime.of(year, month, 1, 0, 0);
+        return dateTime.plusMonths(1);
+    }
+
 }
